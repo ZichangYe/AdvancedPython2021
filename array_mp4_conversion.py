c9404d414ed45db3525e014bcfd2ea935d75e3dd
PygMali0n <54514463+PygMali0n@users.noreply.github.com>
2021-05-02 22:12:16 -0400

    functions convert mp4 and numpy array

---
+++ array_mp4_conversion.py
@@ -0,0 +1,42 @@
+import cv2
+import numpy as np
+
+def array_to_mp4(filename, array, framerate):
+    '''
+    :param filename: path to writen file
+    :param array: 4d numpy array
+    :param framerate: fps
+    '''
+    frame, height, width, channel = array.shape
+    frameSize = (width, height)
+    out = cv2.VideoWriter(filename, cv2.VideoWriter_fourcc(*'mp4v'), framerate, frameSize)
+
+    for i in range(frame):
+        out.write(array[i])
+
+    out.release()
+
+
+def mp4_to_array(filename):
+    '''
+    :param filename: path of file to read
+    :return: 4d numpy array each pixel is bgr
+    '''
+    cap = cv2.VideoCapture(filename)
+    frameCount = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    frameWidth = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    frameHeight = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fps = cap.get(cv2.CAP_PROP_FPS)
+
+    buf = np.empty((frameCount, frameHeight, frameWidth, 3), np.dtype('uint8'))
+
+    fc = 0
+    ret = True
+
+    while (fc < frameCount and ret):
+        ret, buf[fc] = cap.read()
+        fc += 1
+
+    cap.release()
+
+    return buf, fps
