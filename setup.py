39d8f638e2cba030ab72e0547e95a6fd516e7fde
ZichangYe <54966399+ZichangYe@users.noreply.github.com>
2021-05-09 20:35:40 -0400

    delete setup.py for cython

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-from Cython.Build import cythonize
-import numpy
-setup(
-    name='video_transformer_test_cython',
-    ext_modules=cythonize("video_transformer_test_cython.pyx"),
-    zip_safe=False,
-    include_dirs=[numpy.get_include()]
-)
